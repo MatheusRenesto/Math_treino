6b1dfcf37c30650d7b82c96a234de1e3207d8cab
m.renest0 <161990711+MatheusRenesto@users.noreply.github.com>
2025-04-02 19:07:51 +0000

    adiciona o database.js e o compose.yaml

--- pages/api/v1/status/status.js
+++ pages/api/v1/status/status.js
@@ -1,6 +1,10 @@
+import database from "../../../../infra/database.js";
+
 const { exportPages } = require("next/dist/export/worker");
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
   response.status(200).json({ muito: "top" });
 }
 
