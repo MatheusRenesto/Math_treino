3d7fb150cb6385e29f43effa096988d40c75f39f
m.renest0 <matheusfrenesto@gmail.com>
2025-05-05 15:41:05 -0300

    fix: status

--- pages/api/v1/status/status.js
+++ pages/api/v1/status/status.js
@@ -1,11 +1,7 @@
 import database from "../../../../infra/database.js";
 
-const { exportPages } = require("next/dist/export/worker");
-
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ muito: "top" });
+function status(request, response) {
+  response.status(200).json({ chave: "oi" });
 }
 
 export default status;
